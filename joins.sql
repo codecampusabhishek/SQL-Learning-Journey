4820cf35298f9efae0ddfe721d103178ede51012
Abhishek Kumar <Kmkumarak086@gmail.com>
2026-06-23 19:27:23 +0530

    Add files via upload

---
+++ joins.sql
@@ -0,0 +1,13 @@
+USE Abhishek;
+
+SELECT users.id, users.name, addresses.city
+FROM users
+INNER JOIN addresses ON users.id = addresses.user_id;
+
+SELECT users.id, users.name, addresses.city
+FROM users
+LEFT JOIN addresses ON users.id = addresses.user_id;
+
+SELECT users.id, users.name, addresses.city
+FROM users
+RIGHT JOIN addresses ON users.id = addresses.user_id;
